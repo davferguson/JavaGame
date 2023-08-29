d8bcc40eb3f28523971363c63ff99f1acc5921cc
DaredFTW <davferg43@gmail.com>
2023-08-29 15:24:13 -0400

    started init method for WindowManager

--- game-engine-3d/src/main/java/com/gameengine/core/WindowManager.java
+++ game-engine-3d/src/main/java/com/gameengine/core/WindowManager.java
@@ -1,5 +1,67 @@
 package com.gameengine.core;
 
-public class WindowManager {
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryUtil;
 
+public class WindowManager {
+    public static final float FOV = (float) Math.toRadians(60);
+    public static final float Z_NEAR = 0.01f;
+    public static final float Z_FAR = 1000f;
+
+    private final String title;
+
+    private int width, height;
+    private long window;
+
+    private boolean resize, vSync;
+
+    private final Matrix4f projectionMatrix;
+
+    public WindowManager(String title, int width, int height, boolean vSync) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.vSync = vSync;
+        projectionMatrix = new Matrix4f();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        //check if GLFW has been initialized
+        if(!GLFW.glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        GLFW.glfwDefaultWindowHints();
+        //make window invisible
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
+        //make window resizable
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_TRUE);
+        //set recommended version??
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+        //tell openGL what profile to use(CORE_PROFILE)
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+        //any function deprecated in openGL will also be deprecated in the code?
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+
+        boolean maximized = false;
+        if(width == 0 || height == 0) {
+            width = 100;
+            height = 100;
+            GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GLFW.GLFW_TRUE);
+            maximized = true;
+        }
+
+        //create window
+        window = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+        if(window == MemoryUtil.NULL){
+            throw new RuntimeException("Failed to create GLFW window");
+        }
+
+    }
 }
