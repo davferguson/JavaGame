f4b7ece09d5e2947293906f2f1072afe91ff949e
DaredFTW <davferg43@gmail.com>
2023-08-29 16:15:39 -0400

    finished WindowManager and got first window to pop up!

--- game-engine-3d/src/main/java/com/gameengine/core/WindowManager.java
+++ game-engine-3d/src/main/java/com/gameengine/core/WindowManager.java
@@ -3,6 +3,8 @@ package com.gameengine.core;
 import org.joml.Matrix4f;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.system.MemoryUtil;
 
@@ -63,5 +65,115 @@ public class WindowManager {
             throw new RuntimeException("Failed to create GLFW window");
         }
 
+        GLFW.glfwSetFramebufferSizeCallback(window, (window, width, height) -> {
+            this.width = width;
+            this.height = height;
+            this.setResize(true);
+        });
+
+        //check if escape key pressed and close window if it is
+        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if(key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE){
+                GLFW.glfwSetWindowShouldClose(window, true);
+            }
+        });
+
+        if(maximized){
+            GLFW.glfwMaximizeWindow(window);
+        } else {
+            //center window on primary monitor
+            GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            GLFW.glfwSetWindowPos(window, (vidMode.width() - width) / 2, (vidMode.height() - height) / 2);
+        }
+
+        GLFW.glfwMakeContextCurrent(window);
+
+        if(isvSync()){
+            GLFW.glfwSwapInterval(1);
+        }
+
+        //finally show window
+        GLFW.glfwShowWindow(window);
+
+        GL.createCapabilities();
+
+        //set intitial color of window
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_STENCIL_TEST);
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glCullFace(GL11.GL_BACK);
+    }
+
+    public void update() {
+        GLFW.glfwSwapBuffers((window));
+        GLFW.glfwPollEvents();
+    }
+
+    public void cleanup() {
+        GLFW.glfwDestroyWindow(window);
+    }
+
+    public void setClearColour(float r, float g, float b, float a){
+        GL11.glClearColor(r,g,b,a);
+    }
+
+    public boolean isKeyPressed(int keycode) {
+        return GLFW.glfwGetKey(window, keycode) == GLFW.GLFW_PRESS;
+    }
+
+    public boolean windowShouldClose() {
+        return GLFW.glfwWindowShouldClose(window);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title){
+        GLFW.glfwSetWindowTitle(window, title);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public long getWindow() {
+        return window;
+    }
+
+    public Matrix4f getProjectionMatrix() {
+        return projectionMatrix;
+    }
+
+    public boolean isvSync() {
+        return vSync;
+    }
+
+    public void setvSync(boolean vSync) {
+        this.vSync = vSync;
+    }
+
+    public boolean isResize() {
+        return resize;
+    }
+
+    public void setResize(boolean resize) {
+        this.resize = resize;
+    }
+
+    public Matrix4f updateProjectionMatrix() {
+        float aspectRatio = (float) width / height;
+        return projectionMatrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+    }
+
+    public Matrix4f updateProjectionMatrix(Matrix4f matrix, int width, int height) {
+        float aspectRatio = (float) width / height;
+        return matrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
     }
 }
--- game-engine-3d/src/main/java/com/gameengine/launch/Launcher.java
+++ game-engine-3d/src/main/java/com/gameengine/launch/Launcher.java
@@ -1,9 +1,21 @@
 package com.gameengine.launch;
 
+import com.gameengine.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args){
+
         System.out.println(Version.getVersion());
+
+        WindowManager window = new WindowManager("DARED ENGINE", 800, 800, false);
+        window.init();
+
+        //while window not closed, update()
+        while(!window.windowShouldClose()){
+            window.update();
+        }
+
+        window.cleanup();
     }
 }
